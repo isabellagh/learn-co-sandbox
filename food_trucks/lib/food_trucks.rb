87facb6b64dd0a4f5c37b1317309d8c0c3d47387
isabella g henriques <isabellagh88@gmail.com>
2020-06-23 02:22:05 +0000

    indexes

--- food_trucks/lib/food_trucks.rb
+++ food_trucks/lib/food_trucks.rb
@@ -1,7 +1,10 @@
 require_relative "./food_trucks/version"
 require_relative "./food_trucks/cli"
 
-require 'pry'
+
+
+
+
 
 module FoodTrucks
   class Error < StandardError; end
--- food_trucks/lib/food_trucks/cli.rb
+++ food_trucks/lib/food_trucks/cli.rb
@@ -2,6 +2,25 @@ class FoodTrucks::CLI
   
   def call 
     puts "Welcome to Baltimore Food Trucks"
+    get_food_types
+    get_user_food_type
+    #food_type(prefered, vegetarian)
+    #list_prefered
+    #list_vegetarian
+    
+  end 
+  
+  def get_food_types 
+    #to be scraped
+    @types = ["prefered", "savory", "sweet", "vegetarian"]
+  end 
+  
+  def get_user_food_type
+    #get user types 
+    @types.each_with_index { |type,index|
+      puts "#{index + 1}- #{type}"
+    }
+    
   end 
   
 end 
