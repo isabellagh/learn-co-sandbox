2c569e2023ed76e29c273294788b5595b276a8f1
isabella g henriques <isabellagh88@gmail.com>
2020-06-23 20:20:41 +0000

    little

--- food_trucks/lib/food_trucks/cli.rb
+++ food_trucks/lib/food_trucks/cli.rb
@@ -10,21 +10,19 @@ class FoodTrucks::CLI
   
   def get_food_types 
     #to be scraped
-    puts "What type of Food Truck would you like to see, 1 or 2?"
     @types = ["21 Prefered", "Vegetarian"]
   end 
   
   def truck_types
-    #get user types 
+    puts "What type of Food Truck would you like to see, 1 or 2?"
     @types.each.with_index(1) { |type,index|
       puts "#{index}- #{type}"
     }
   end 
   
   def get_user_choice
-    chosen_types = gets.strip
-    #if
-    #end 
+    chosen_types = gets.strip.to_i
+    show_trucks_for(chosen_types) if valid_input(chosen_types, @types)
     
   end 
   
