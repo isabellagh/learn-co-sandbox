b2c6c39c70f62ed7e6334251d82f20079bc85cc5
isabella g henriques <isabellagh88@gmail.com>
2020-06-24 02:18:37 +0000

    add code

--- food_trucks/lib/food_trucks/cli.rb
+++ food_trucks/lib/food_trucks/cli.rb
@@ -2,47 +2,38 @@ class FoodTrucks::CLI
   
   def call 
     puts "\nWelcome to Baltimore Food Trucks!\n"
-    start
     
-    print_food_trucks(truck)
-    
-  end 
-  
-  def start
-    puts "What number food trucks would you like to see? 1 or 2?"
-    input = gets.strip.to_i
+    get_food_types
+    truck_types
+    get_user_choice
     
   end 
   
-  def print_food_trucks(truck)
+  def get_food_types 
+    #to be scraped
+    @types = ["21 Prefered", "Vegetarian"]
+  end 
+  
+  def truck_types
+    puts "What type of Food Truck would you like to see, 1 or 2?"
+    @types.each.with_index(1) { |type, index|
+      puts "#{index}- #{type}"
+    }
+  end 
+  
+  def get_user_choice
+    chosen_type = gets.strip.to_i
+    show_trucks_for(chosen_type) if valid_input(chosen_type, @types)
     
   end 
   
-  #def get_food_types 
-  #  #to be scraped
-  #  @types = ["21 Prefered", "Vegetarian"]
-  #end 
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0 
+  end 
   
-  #def truck_types
-  #  puts "What type of Food Truck would you like to see, 1 or 2?"
-  #  @types.each.with_index(1) { |type, index|
-  #    puts "#{index}- #{type}"
-  #  }
-  #end 
-  
-  #def get_user_choice
-  #  chosen_type = gets.strip.to_i
-  #  show_trucks_for(chosen_type) if valid_input(chosen_type, #@types)
-    
- # end 
-  
- # def valid_input(input, data)
- #   input.to_i <= data.length && input.to_i > 0 
- # end 
-  
- # def show_trucks_for(chosen_type)
- #   type = @type[chosen_type -1]
- #     puts "Here are the trucks for #{type}:"
- # end 
+  def show_trucks_for(chosen_type)
+    type = @types[chosen_type -1]
+      puts "Here are the trucks for #{type}:"
+  end 
   
 end 
